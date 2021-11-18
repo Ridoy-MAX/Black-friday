4e0535ffd9909307399ea38dcaf07701997b830a
Ridoy-MAX <pou4w98y@gmail.com>
2021-11-18 20:11:30 +0600

    responsive

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,23 @@
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+  const header = document.getElementById('menu')
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+/*==================== CHANGE BACKGROUND HEADER end ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 300) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+
 $('.banner-slider').slick({
   slidesToShow: 4,
   slidesToScroll: 1,
@@ -80,3 +100,13 @@ $('.owl-carousel').slick({
   periodInterval: 10,
   periodUnit: "d",
 });
+
+
+
+// mega menu
+$('.js-mega-menu').HSMegaMenu({
+  event: 'hover',
+  pageContainer: $('.container'),
+  breakpoint: 767.98,
+  hideTimeOut: 0
+}); 
