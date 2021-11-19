7ef59725dcaa521519ff6109d3131587d171e245
Ridoy-MAX <pou4w98y@gmail.com>
2021-11-19 22:27:59 +0600

    pricing plan add

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,11 @@
-/*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
-  const header = document.getElementById('menu')
-  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-  if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
-/*==================== CHANGE BACKGROUND HEADER end ====================*/
+// 9. client-testimonial one item carousel
 
-/*==================== SHOW SCROLL UP ====================*/ 
-function scrollUp(){
-  const scrollUp = document.getElementById('scroll-up');
-  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if(this.scrollY >= 300) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-
-$('.banner-slider').slick({
-  slidesToShow: 4,
+$('.banner-slider1').slick({
+  slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
+  dots:false,
+  arrows:false,
   autoplaySpeed: 2000,
   nextArrow:'<i class="fas fa-chevron-left  next-arrow"></i>',
   prevArrow:'<i class="fas fa-chevron-right  previous-arrow"></i>',
@@ -52,6 +36,33 @@ $('.banner-slider').slick({
     // instead of a settings object
   ]
 });
+
+// 1. preloader
+
+$(window).ready(function () {
+  $('#preloader').delay(200).fadeOut('fade');
+});
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+  const header = document.getElementById('menu')
+  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+/*==================== CHANGE BACKGROUND HEADER end ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 300) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+
+
 
 
 
